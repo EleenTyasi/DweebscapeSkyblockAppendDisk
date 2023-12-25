4ce900f152c050864713f18a1904cf581b2201f4
EleenTyasi <106459601+EleenTyasi@users.noreply.github.com>
2023-12-25 04:21:28 -0800

    initial

---
+++ src/main/java/net/mcreator/dwbscpsad/procedures/ApndSoulsandProcProcedure.java
@@ -0,0 +1,52 @@
+package net.mcreator.dwbscpsad.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.TickEvent;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class ApndSoulsandProcProcedure {
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase == TickEvent.Phase.END) {
+			execute(event, event.player);
+		}
+	}
+
+	public static void execute(Entity entity) {
+		execute(null, entity);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity) {
+		if (entity == null)
+			return;
+		if (new Object() {
+			public boolean hasRecipe(Entity _ent, ResourceLocation recipe) {
+				if (_ent instanceof ServerPlayer _player)
+					return _player.getRecipeBook().contains(recipe);
+				else if (_ent.level().isClientSide() && _ent instanceof LocalPlayer _player)
+					return _player.getRecipeBook().contains(recipe);
+				return false;
+			}
+		}.hasRecipe(entity, new ResourceLocation("dwbscpsad:apnd_soulsand"))) {
+			if (entity instanceof ServerPlayer _player) {
+				Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("dwbscpsad:apnd_soulsand_adv"));
+				AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+				if (!_ap.isDone()) {
+					for (String criteria : _ap.getRemainingCriteria())
+						_player.getAdvancements().award(_adv, criteria);
+				}
+			}
+		}
+	}
+}
